380399479eb3cf4111d64a7bdb4ad65e4001d09c
emachipana <emachipana0102@gmail.com>
2022-06-03 02:07:59 -0500

    FIX: condition by localstorage

--- scripts/pages/homePagesHandler.js
+++ scripts/pages/homePagesHandler.js
@@ -1,10 +1,8 @@
 export const homePagesKey = "homePagesHanlder";
-localStorage.setItem(homePagesKey, JSON.stringify({current_page: ""}));
-
 const actualPage = JSON.parse(localStorage.getItem(homePagesKey));
 
 const HomePagesHanlder = {
-    currentPage: actualPage.current_page || "myBoards",
+    currentPage: actualPage ? actualPage.current_page : "myBoards",
     setCurrentPage(page) {
         this.currentPage = page;
         const data = { current_page: page }
