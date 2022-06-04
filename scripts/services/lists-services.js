e13dfa5331653714350a21621d57040846542977
emachipana <emachipana0102@gmail.com>
2022-06-04 09:54:50 -0500

    FIX: add key word async

--- scripts/services/lists-services.js
+++ scripts/services/lists-services.js
@@ -5,18 +5,19 @@ export function getLists(board) {
     return board.lists;
 }
 
-export function createList(boardId, newList = { listName }) {
+export async function createList(boardId, newList = { listName }) {
     const listCreated = await apiFetch(`boards/${boardId}/lists`, { body: newList });
 
     return listCreated;
 }
 
-export function updateList(boardId, listId, payload = { listName }) {
+export async function updateList(boardId, listId, payload = { listName }) {
     const listUpdated = await apiFetch(`boards/${boardId}/lists/${listId}`, { method: "PATCH", body: payload });
 
     return listUpdated;
 }
 
-export function deleteList(boardId, listId) {
+export async function deleteList(boardId, listId) {
     await apiFetch(`boards/${boardId}/lists/${listId}`, { method: "DELETE" });
 }
+
