b89cd060539ed4bae84fdb0cbd1ac4663a6e0d28
emachipana <emachipana0102@gmail.com>
2022-06-04 11:09:35 -0500

    ADD: listen back button function

--- scripts/pages/listsPage.js
+++ scripts/pages/listsPage.js
@@ -1,11 +1,42 @@
+import DOMHandler from "../dom-handler.js";
+import STORE from "../store.js";
+import HomePage from "./homePage.js";
+
 function render() {
+    const board = STORE.currentBoard || JSON.parse(localStorage.getItem("currentBoard"));
+    const lists = STORE.currentLists || JSON.parse(localStorage.getItem("currentLists"));
+    
     return `
         <main class="lists-section">
             <header class="lists__header">
+                <a href="#" class="back-button">
+                    <img src="./assets/icons/back.svg" alt="back-icon" />
+                    <p>Back</p>
+                </a>
                 <img src="./../assets/images/logo.png" class="logo" alt="logo-image">
             </header>
+            <div class="lists__container ${board.color}">
+                <header>
+                    <h1 class="lists__title">${board.name}</h1>
+                </header>
+                <div class="lists">
+
+                </div>
+            </div>
         </main>
     `
+}
+
+function listenBackButton() {
+    const button = document.querySelector(".back-button");
+    const root = document.querySelector("#root");
+
+    button.addEventListener("click", e => {
+        e.preventDefault();
+
+        STORE.setCurrentPage("boards");
+        DOMHandler.load(HomePage(), root);
+    })
 }
 
 function ListPage() {
@@ -14,7 +45,7 @@ function ListPage() {
             return render();
         },
         addListeners() {
-
+            listenBackButton();
         }
     }
 }
